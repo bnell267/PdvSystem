de266431506af7806ee91172b2a38b246033b86f
bnell267 <jessepereira170@gmail.com>
2025-01-27 08:25:29 -0300

    Update config2.php

--- config2.php
+++ config2.php
@@ -1,9 +1,13 @@
 
 <?php
-$dbHost ='sql107.infinityfree.com';
-$dbUsername = 'if0_38152063';
-$dbPassword = 'ajpda135A';
-$dbName = 'if0_38152063_teste';
+$dbHost ='localhost';
+  //'sql107.infinityfree.com';
+$dbUsername ='root'; 
+  //'if0_38152063';
+$dbPassword = '';
+  //'ajpda135A';
+$dbName = 'teste';
+  //'if0_38152063_teste';
 
 
 $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
